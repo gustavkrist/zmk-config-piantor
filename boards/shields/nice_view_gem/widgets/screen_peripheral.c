b9738c5b5f5da0abcd420cd681de0d4870f8aab9
Gustav Kristensen <gustavkrist@gmail.com>
2026-03-21 20:50:58 +0100

    fix image alignment

--- boards/shields/nice_view_gem/widgets/screen_peripheral.c
+++ boards/shields/nice_view_gem/widgets/screen_peripheral.c
@@ -116,7 +116,7 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     // draw_animation(widget->obj);
     lv_obj_t *art = lv_img_create(widget->obj);
     lv_img_set_src(art, &through_the_silence);
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 36, 0);
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
